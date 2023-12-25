2c1b0b5a9d7e089587cb972a1d85015c7967554b
GilbertoCI <150563061+GilbertoCI@users.noreply.github.com>
2023-12-25 18:12:04 -0300

    Adicionado a interface de vetorização

--- classifier/gpel_text_dataset.py
+++ classifier/gpel_text_dataset.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import spacy
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.model_selection import train_test_split
 
-
-class GpelTextDataset
+class GpelTextDataset:
     '''
     Classe que manipula os dados de texto do datset GPEL
     '''
-    def __init__(self):
-        pass
+    def __init__(self, size_test : float = 0.25):
+        self.X : list = None
+        self.y : list = None
+        self.X_train : list = None
+        self.y_train : list = None
+        self.X_test : list = None
+        self.y_test : list = None
+        
+        self._load()
+        self._preprocessing()
+        self._vectorization()
+        self._split_train_test(size_test)
     
-    def load(self) -> None:
+    def _load(self) -> None:
         '''
         Carrega todos os arquivos de texto (.txt) do dataset GPEL
         e adiciona na matriz de textos X associando o mesmo ao rótulo
@@ -22,10 +34,10 @@ class GpelTextDataset
         '''
         pass
     
-    def preprocessing(self) - > None:
+    def _preprocessing(self) -> None:
         '''
         Tratamento de PLN sobre os textos:
-            1. Lenatização das plavras;
+            1. Lenatização das plavras; (spacy)
             2. Remoção dos acentos (minimiza erros do OCR);
             3. Remoção das stop words (lenatizadas e sem acento);
             4. Remoção das palavras frequentes no vocabulário. Para a remoção
@@ -50,4 +62,44 @@ class GpelTextDataset
         pass
 
 
-    def 
+        
+    def _vectorization(self):
+        '''
+          Converte o texto em uma representação numérica que os algoritmos de 
+          aprendizado de máquina possam entender.
+          
+          Cada texto é representado por um vetor no qual cada componente 
+          corresponde à frequência de uma palavra específica no vocabulário.
+          Esse método é conhecido como Bag of Words. E pode ser implementado 
+          com a classe DictVectorizer do pacote sklearn.feature_extraction.
+          
+          Outras técnicas de vetorização incluem o uso de esquemas mais avançados, 
+          como o TF-IDF (Term Frequency-Inverse Document Frequency) ou a representação 
+          de palavras usando embeddings, como Word2Vec ou GloVe.
+          
+
+        Returns
+        -------
+        None.
+
+        '''
+        pass
+        
+    def _split_train_test(self, test_size : float):
+        '''
+        Quebra a instância X em X_train, usado na fase de aprendizado do dados, e em X_test
+        a ser usada na computação das métricas de classificação e comparação com outros modelos.
+
+        Parameters
+        ----------
+        test_size : float
+            Indica a proporção de dados da matriz X que irá compor a matriz X_test, 
+            sendo o resto colocado na matriz X_train.
+
+        Returns
+        -------
+        None.
+
+        '''
+        pass
+        
